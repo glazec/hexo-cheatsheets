b6951cb28c23e054344fc2967770663baefca713
glaze <luyiping1011@gmail.com>
2020-04-04 21:23:44 -0700

    build: add purgecss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 require('style-loader')
 require('css-loader')
 require('sass-loader')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    mode:'development',
     entry: ['./source/sass/2017/style.scss'],
     output: {
         path: path.resolve(__dirname, 'source/css'),
@@ -12,6 +14,7 @@ module.exports = {
       },
       plugins: [
         new MiniCssExtractPlugin(),
+        new WebpackShellPlugin({onBuildEnd:['npx purgecss --output source/css/ --content ../../public/*.html ../../public/**/*.html source/js/*.js --css source/css/main.css']})
       ],
     module: {
       rules: [
