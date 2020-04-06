876604e01dae961d0749b25117bb0e8120bacdb5
glaze <luyiping1011@gmail.com>
2020-04-06 13:49:49 -0700

    perf: ⚡️ move part of the js glue to hexo filter
    delete console.js

---
+++ scripts/filter.js
@@ -0,0 +1,10 @@
+// add language- to code class
+hexo.extend.filter.register('before_post_render', function(data){
+    // console.log('filter excute')
+    // console.log(data.content)
+    data.content  = data.content.replace(/```\w{1,}/g,str=>{
+        return '```'+'language-'+str.toString().split('```')[1];
+    })
+    console.log(data.content)
+    return data
+  });
