887b46a7cb662f9728938cd288da1baed424ef9e
glaze <luyiping1011@gmail.com>
2020-04-12 19:22:31 -0700

    style: 💄 auto fix js style [skip ci]

--- source/js/article.js
+++ source/js/article.js
@@ -19,112 +19,110 @@
 // })();
 
 /* high light prism js */
-(function() {
+(function () {
   Prism.highlightAll();
-})();
+}());
 
 /* add .h2-section */
-(function() {
-  var parent = document.querySelectorAll("main")[0];
+(function () {
+  const parent = document.querySelectorAll('main')[0];
   elements = [...parent.childNodes];
-  for (var i = 0; i < elements.length; i++) {
-    var el = elements[i];
-    if (el.nodeName == "H2") {
-      var wrapper = document.createElement("div");
-      wrapper.setAttribute("class", "h2-section");
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    if (el.nodeName == 'H2') {
+      const wrapper = document.createElement('div');
+      wrapper.setAttribute('class', 'h2-section');
       parent.replaceChild(wrapper, el);
       wrapper.appendChild(el);
-      i = i + 1;
-      while (i < elements.length && elements[i].nodeName != "H2") {
+      i += 1;
+      while (i < elements.length && elements[i].nodeName != 'H2') {
         wrapper.append(elements[i]);
         i++;
       }
-      i = i - 1;
+      i -= 1;
     }
   }
-})();
+}());
 
 /* add # to h2 */
-(function() {
-  var elements = document.querySelectorAll("h2>a")
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].text="#"
-    elements[i].className = elements[i].className+" local-anchor anchor"
+(function () {
+  const elements = document.querySelectorAll('h2>a');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].text = '#';
+    elements[i].className = `${elements[i].className} local-anchor anchor`;
   }
-})();
+}());
 
 /* add .h3-section */
-(function() {
-  var elements = document.querySelectorAll("h3");
-  for (var i = 0; i < elements.length; i++) {
-    var el = elements[i];
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "h3-section");
-    var parent = el.parentNode;
-    var childNode = checkNextSibling(el);
+(function () {
+  const elements = document.querySelectorAll('h3');
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'h3-section');
+    const parent = el.parentNode;
+    const childNode = checkNextSibling(el);
     parent.replaceChild(wrapper, el);
     wrapper.append(elements[i]);
-    for (var ei = 0; ei < childNode.length; ei++) {
+    for (let ei = 0; ei < childNode.length; ei++) {
       wrapper.appendChild(childNode[ei]);
     }
   }
 
   function checkNextSibling(el) {
-    var sibling = el.nextElementSibling;
-    if (sibling && sibling.nodeName != "H3" && sibling.nodeName != "H2") {
+    const sibling = el.nextElementSibling;
+    if (sibling && sibling.nodeName != 'H3' && sibling.nodeName != 'H2') {
       return [sibling].concat(checkNextSibling(sibling));
-    } else {
-      return [];
     }
+    return [];
   }
-})();
+}());
 
 /* add .h3-section list */
-(function() {
-  var elements = document.querySelectorAll("div .h3-section");
-  for (var i = 0; i < elements.length; i++) {
-    var el = elements[i];
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "h3-section-list -three-column");
-    var parent = el.parentNode;
-    var childNode = checkNextSibling(el);
+(function () {
+  const elements = document.querySelectorAll('div .h3-section');
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'h3-section-list -three-column');
+    const parent = el.parentNode;
+    const childNode = checkNextSibling(el);
     parent.replaceChild(wrapper, el);
     wrapper.append(elements[i]);
-    for (var ei = 0; ei < childNode.length; ei++) {
+    for (let ei = 0; ei < childNode.length; ei++) {
       i++;
       wrapper.appendChild(childNode[ei]);
     }
   }
 
   function checkNextSibling(el) {
-    var sibling = el.nextElementSibling;
-    if (sibling && sibling.classList.value.toString().includes("h3-section")) {
+    const sibling = el.nextElementSibling;
+    if (sibling && sibling.classList.value.toString().includes('h3-section')) {
       return [sibling].concat(checkNextSibling(sibling));
-    } else {
-      return [];
     }
+    return [];
   }
-})();
+}());
 
 /* add .-headers to table */
-(function() {
-  document.querySelectorAll("table").forEach(function(node) {
-    node.classList.add("-headers");
+(function () {
+  document.querySelectorAll('table').forEach((node) => {
+    node.classList.add('-headers');
   });
-})();
+}());
 
 /* add .body */
-(function() {
-  var elements = document.querySelectorAll("div .h3-section");
-  for (var i = 0; i < elements.length; i++) {
-    var parent = elements[i];
-    var wrapper = document.createElement("div");
-    wrapper.setAttribute("class", "body");
-    var childNode = [...parent.childNodes];
+(function () {
+  const elements = document.querySelectorAll('div .h3-section');
+  for (let i = 0; i < elements.length; i++) {
+    const parent = elements[i];
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'body');
+    const childNode = [...parent.childNodes];
     el = childNode[1];
     parent.replaceChild(wrapper, el);
-    for (var ei = 1; ei < childNode.length; ei++) {
+    for (let ei = 1; ei < childNode.length; ei++) {
       wrapper.appendChild(childNode[ei]);
     }
   }
-})();
+}());
