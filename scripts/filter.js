3f568e831ea6e3a2ef4a52ad6ac35700ebadc00b
mahoo12138 <mahoo12138@qq.com>
2022-08-11 23:54:24 +0800

    chore: update the prismjs

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,8 +1,8 @@
-// add language- to code class
-hexo.extend.filter.register('before_post_render', (data) => {
-  // console.log('filter excute')
-  // console.log(data.content)
-  data.content = data.content.replace(/```\w{1,}/g, (str) => `${'```' + 'language-'}${str.toString().split('```')[1]}`);
-  console.log(data.content);
-  return data;
-});
+// // add language- to code class
+// hexo.extend.filter.register('before_post_render', (data) => {
+//   // console.log('filter excute')
+//   // console.log(data.content)
+//   data.content = data.content.replace(/```\w{1,}/g, (str) => `${'```' + 'language-'}${str.toString().split('```')[1]}`);
+//   console.log(data.content);
+//   return data;
+// });
